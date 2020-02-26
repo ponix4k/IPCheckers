ac2866b3a7a8ff65b35e66548f6def7bd2ec826a
ponix4k <2320804+ponix4k@users.noreply.github.com>
2020-02-26 17:33:55 +0000

    Update to main and removed email

--- main.py
+++ main.py
@@ -6,12 +6,46 @@ from datetime import datetime
 
 contact_method_r = open("settings.conf","r")
 contact_method = contact_method_r.readlines()[0]
-sttime = datetime.now().strftime("%Y%m%d_%H:%M:%S - ")
+sttime = datetime.now().strftime("%Y-%m-%d__%H:%M:%S - ")
 user = "pi"
 path ="/home/"+user+"/IPCheckers"
 Last_IP = path + "/ips/lastip.txt"
 Current_IP = path + "/ips/currentip.txt"
 Same_IP = False
+
+def Check_Settings():
+    if os.path.isfile("settings.conf"):
+        user_settings = open("settings.conf","r")
+        contact_method = user_settings.readlines()[0]
+        print(contact_method.lower())
+        if contact_method.lower() == "email":
+            email_username = user_settings.readlines()[1]
+            if email_username == "":
+                print("No username storeed check your settings.conf file")
+            else:
+                print (email_username)
+                email_password = user_settings.readlines()[2]
+                if email_password == "":
+                    print("No password has been saved")
+        else:
+            print ("No contact method selected")
+    else:
+        print ("Error no config file found")
+        create_config = input ("Do you wish to Create a config file (Y or N): ")
+        if create_config.upper == "Y":
+            f = open("settings.conf","a+")
+            email_username = input("enter username: ")
+            email_password = input("Enter Password: ")
+            f.write(str(email_username)+"\n")
+            f.write(str(email_password))
+            f.close()
+            print ("config file created ")
+        else:
+            print ("Exiting program")
+                
+        
+        
+    Check_Settings()
 
 def Last_IP_Check():
     if os.path.isfile(Last_IP):
@@ -53,62 +87,40 @@ def Get_Current_IP():
     f2.close
 
 def Email():
-    if os.path.isfile("settings.conf"):
-        email_address_r = open("settings.conf","r")
-        email_username = email_address_r.readlines()[1]
-        if email_username == "":
-            print("No Email Stored check your settings.conf file")
-        else:
-            password_r = open("settings.conf","r")
-            email_password = password_r.readlines()[2]
-            if email_password == "":
-                print("No Password has been stored check your settings.conf file")
-            else:
-                f = open("ips/currentip.txt","r")
-                IP_Address = f.read()
-                sent_from = email_username
-                sent_to = email_username
-                subject = "IP Address has changed"
-                body = "Your new IP address is : " + str(IP_Address)
-                email_text = """\
-                        From: %s
-                        To: %s
-                        Subject: %s
-                        %s
-                        """ % (sent_from, ", ".join(sent_to), subject, body)
-                try:
-                            server = smtplib.SMTP_SSL("smtp.gmail.com",465)
-                            server.ehlo()
-                            server.login(email_username, email_password)
-                            server.sendmail(sent_from,sent_to,email_text)
-                            server.close()
-                            print ("Email Has Been Sent")
-                except:
-                            print ("Something went wrong...")
-    else:
-        print ("Error no config file found")
-        create_config = input ("Do you wish to Create a config file (Y or N): ")
-        if create_config == "Y":
-            f = open("settings.conf","a+")
-            email_username = input("enter username: ")
-            email_password = input("Enter Password: ")
-            f.write(str(email_username)+"\n")
-            f.write(str(email_password))
-            f.close()
-            print ("config file created ")
-        else:
-            print ("Exiting program")
+            user_settings = open("settings.conf","r")
+            email_username = user_settings.readlines()[1]
+            email_password = user_settings.readlines()[2]            
+            f = open("ips/currentip.txt","r")
+            IP_Address = f.read()
+            sent_from = email_username
+            sent_to = email_username
+            subject = "IP Address has changed"
+            body = "Your new IP address is : " + str(IP_Address)
+            email_text = """\
+                    From: %s
+                    To: %s
+                    Subject: %s
+                    %s
+                    """ % (sent_from, "".join(sent_to), subject, body)
+            try:
+                        server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+                        server.ehlo()
+                        server.login(email_username, email_password)
+                        server.sendmail(sent_from,sent_to,email_text)
+                        server.close()
+                        print ("Email Has Been Sent")
+            except:
+                        print ("Something went wrong...")
+
             
 def Matrix():
     print("Using Matrix")
 
-
 def SMS():
     print("Using SMS")
-
-
     
 def Main():
+    Check_Settings()
     Current_IP_Check()
     Last_IP_Check()
     f1 = open(Current_IP,"r")
