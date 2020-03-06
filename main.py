0f9657e18000c06c28fb80eb8146d2de8912f903
ponix4k <ponix@protonmail.com>
2020-03-06 18:56:43 +0000

    Created install files

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-import smtplib 
+import smtplib
 import os,sys
+import xml.dom.minidom
+from datetime import datetime
+
+timestamp = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
+
 log_path = "log.txt"
-IP_log = "ip.txt"
+IP_log = "ips/last_ip.txt"
 
 class notification():
     message = ""
@@ -15,12 +20,11 @@ class notification():
 
 class email_notification(notification):
     import smtplib
-    
+
     def __init__(self, message, settings):
         self.email_user = settings["email_user"]
         self.email_password = settings["email_password"]
         self.message = message
-        
         self.to = settings["to"]
         self.subject = settings["subject"]
         self.email_server = settings["email_server"]
@@ -37,11 +41,8 @@ class email_notification(notification):
         except:
            print("shit went wrong")
 
-
 def send(notif):
     notif.send_message()
-
-
 
 def check_setting_is_empty(setting):
     if len(setting > 0):
@@ -50,14 +51,13 @@ def check_setting_is_empty(setting):
         return True
 
 def create_settings_file(path, settings):
-    settings["contact_method"] = input("enter contact method:").lower()
-    settings["email_user"] = input("enter email user:").lower()
-    settings["email_password"] = input("enter password:")
-    settings["to"] = input("enter recipient:")
+    settings["contact_method"] = input("enter contact method: ").lower()
+    settings["email_user"] = input("enter email user: ").lower()
+    settings["email_password"] = input("enter password: ")
+    settings["to"] = input("enter recipient: ")
     settings["email_server"] = "smtp.gmail.com"
     settings["email_port"] = 465
     settings["subject"] = "IP Address Update"
-
     with open(path, 'a+') as settings_file:
         for key,value in settings.items():
             settings_file.write("{}\n".format(value))
@@ -65,13 +65,11 @@ def create_settings_file(path, settings):
     settings_file.close()
     return settings
 
-
 def load_settings(path):
     settings = {}
     if os.path.isfile(path):
         print("file found")
         with open(path, 'r') as settings_file:
-            
             settings["contact_method"] = settings_file.readlines()[0].lower()
             if (settings["contact_method"] == "email"):
                 settings["email_user"] = settings_file.readlines()[1]
@@ -82,7 +80,6 @@ def load_settings(path):
                     print("no password found in config file")
             else:
                 print("no contact method selected")
-            
             print(settings)
             settings_file.close()
     else:
@@ -95,6 +92,7 @@ def get_IP_address():
 def read_IP_log():
     with open(IP_log) as IPlog:
         logged_IP = IPlog.read()
+        print ("Logged IP is: " +logged_IP)
     IPlog.close()
     return logged_IP
 
@@ -102,15 +100,14 @@ def get_timestamp():
     return "2020/02/27" # make this a real date obviously
 
 def log_IP_change(old_IP, new_IP):
+    print("old IP is: "+old_IP,"new ip is: "+new_IP)
     with open(log_path, "a+") as log:
         log.write("[{}] <INFO> IP Address Changed! Updating\n".format(get_timestamp()))
         log.write("[{}] <INFO> New Address: {}\n\t\t Old Address:{}".format(get_timestamp(), new_IP, old_IP))
         with open(IP_log, "w") as iplog:
-            iplog.write(new_IP)            
-        iplog.close()
-    log.close()
-
-
+            iplog.write(new_IP)
+            iplog.close()
+        log.close()
 
 def get_current_IP_address():
     current_IP = get_IP_address()
@@ -122,10 +119,15 @@ def get_current_IP_address():
     else:
         log_IP_change("0.0.0.0",current_IP)
         return current_IP
-    
+        print (current_IP)
+
 def main():
+    currentip = get_current_IP_address()
+    print(currentip)
     settings = load_settings("settings.conf")
-    printnotif= notification("your IP is {}".format(get_current_IP_address()))
+    print (settings)
+    printnotif = notification("your IP is: " + str(currentip))
+    print (printnotif)
     #email= email_notification("your IP is {}".format(get_current_IP_address()), settings)
     send(printnotif)
     #send(email)
