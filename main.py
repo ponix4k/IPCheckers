8cd3722a4080a703e310000572874d72a09871fd
ponix4k <2320804+ponix4k@users.noreply.github.com>
2020-03-08 23:12:08 +0000

    Added json settings reader

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import smtplib
 import os,sys
-import xml.etree.ElementTree as ET
+import json
 from datetime import datetime
 
 timestamp = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
-
-log_path = "log.txt"
-IP_log = "ips/last_ip.txt"
-
+config = 'settings.json'
+log_path = 'log.txt'
+IP_log = 'ips/last_ip.txt'
+global has_changed 
 class notification():
     message = ""
 
@@ -17,18 +17,20 @@ class notification():
 
     def send_message(self):
         print(self.message)
-
+        
 class email_notification(notification):
-    import smtplib
-
-    def __init__(self, message, settings):
-        self.email_user = settings["email_user"]
-        self.email_password = settings["email_password"]
-        self.message = message
-        self.to = settings["to"]
+    
+    def __init__ (self, ip, settings):
+        print(settings)
+        self.email_user = settings["username"]
+        self.email_password = settings["password"]
+        self.message = ["message"]
+        self.to = settings["username"]
         self.subject = settings["subject"]
         self.email_server = settings["email_server"]
         self.email_port = settings["email_port"]
+        self.ip = ip
+        
 
     def send_message(self):
         try:
@@ -43,51 +45,80 @@ class email_notification(notification):
 
 def send(notif):
     notif.send_message()
+    
+def write_default_settings():
+    settings = {}
+    settings['email'] = {
+            "name": "email",
+            "id": "1",
+            "is_enabled": True,
+            "username": "email@email.com",
+            "password": "hunter2",
+            "subject": "IP HAS CHANGED",
+            "message":"Your new IP address is: ",
+            "email_server": "smtp.gmail.com",
+            "email_port": "465" 
+    }
+    settings['matrix'] = {
+            "name": "matrix",
+            "id": "2",
+            "is_enabled": False,
+            "username": "email@email.com",
+            "password": "hunter2",
+            "subject": "IP HAS CHANGED",
+            "message":"Your new IP address is: ",
+            "email_server": "smtp.gmail.com",
+            "email_port": "465" }
+ 
 
-def check_setting_is_empty(setting):
-    if len(setting > 0):
-        return False
+    s=json.dumps(settings)
+    with open (config,'w') as f:
+        f.write(s)
+
+def read_settings():
+    if os.path.isfile(config):
+        file=open(config,'r')
+        setting_file=file.read()
+        settings = json.loads(setting_file)
     else:
-        return True
-
-def create_settings_file(path, settings):
-    settings["contact_method"] = input("enter contact method: ").lower()
-    settings["email_user"] = input("enter email user: ").lower()
-    settings["email_password"] = input("enter password: ")
-    settings["to"] = input("enter recipient: ")
-    settings["email_server"] = "smtp.gmail.com"
-    settings["email_port"] = 465
-    settings["subject"] = "IP Address Update"
-    with open(path, 'a+') as settings_file:
-        for key,value in settings.items():
-            settings_file.write("{}\n".format(value))
-
-    settings_file.close()
+        create_file = input("No File found do you wish to create one ? (Y or N): ").upper()
+        if create_file == 'Y':
+            default = input("Do you want to use the default layout ? (Y or N): ").upper()
+            if default == 'Y':
+                write_default_settings()
+            else:
+                 write_settings()
+        else: 
+            print("Exiting Program")
+            sys.exit()
     return settings
-
-def load_settings():
-    if os.path.isfile('test_settings.xml'):
-        print ("Settings File Found")
-        tree = ET.parse('test_settings.xml')
-        root = tree.getroot()
-        for settings in root.findall('contact_method'):
-                att = settings.attrib
-                #print(att)
-                name=att.get('name')
-                enabled =att.get('is_enabled')
-                #print(enabled)
-                if enabled == 'True':
-                    print(name,': ',enabled)
-                    username=settings.find('username').text
-                    password=settings.find('password').text
-                    email_server=settings.find('email_server').text
-                    email_port=settings.find('email_port').text
-                    subject=settings.find('subject').text
-                    print ('#################\n',username,':',password,'\n',email_server,':',email_port,'\n',subject,'\n#################')
-    else:
-        print ("No settings file could be located")
-load_settings()
-
+            
+def write_settings():
+    settings = {}
+    name = input('Please enter the name of the contact method: ')
+    enabled = input('Do you want this method to be enabled(true or false): ')
+    username = input('Please eenter desired username: ')
+    passwd = input('Please enter desired password: ')
+    subject = input('Please enter desired subject: ')
+    message = input('Please enter the desired message: ')
+    server = input('Please enter the address of the server you wish to user: ')
+    port = input('Please set the port you wish to use: ')
+    
+    settings['email'] = {
+            "name": name,
+            "id": "1",
+            "is_enabled": enabled,
+            "username": username,
+            "password": passwd,
+            "subject": subject,
+            "message": message,
+            "email_server": server,
+            "email_port": port 
+    }
+    setting_file=json.dumps(settings)
+    with open (config,'w') as file:
+        file.write(setting_file)
+  
 def get_IP_address():
     return os.popen("curl ifconfig.me").read()
 
@@ -102,7 +133,6 @@ def get_timestamp():
     return "2020/02/27" # make this a real date obviously
 
 def log_IP_change(old_IP, new_IP):
-    print("old IP is: "+old_IP,"new ip is: "+new_IP)
     with open(log_path, "a+") as log:
         log.write("[{}] <INFO> IP Address Changed! Updating\n".format(get_timestamp()))
         log.write("[{}] <INFO> New Address: {}\n\t\t Old Address:{}".format(get_timestamp(), new_IP, old_IP))
@@ -115,24 +145,28 @@ def get_current_IP_address():
     current_IP = get_IP_address()
     if os.path.isfile(IP_log):
         logged_IP = read_IP_log()
-        if not (logged_IP == current_IP): #if they don't match IP has changed - do stuff
-            log_IP_change(logged_IP, current_IP)
+        if (logged_IP == current_IP): #if they don't match IP has changed - do stuff
+            has_changed = False
             return current_IP
+        else:
+           log_IP_change(logged_IP, current_IP)
+           print(logged_ip,current_ip)
+           has_changed = True
     else:
         log_IP_change("0.0.0.0",current_IP)
+        has_changed = True
         return current_IP
-        print (current_IP)
-
+        
 def main():
-    settings = load_settings()
-    #currentip = get_current_IP_address()
-    #print(currentip)
+    has_changed = False
+    settings = read_settings()
+    currentip = get_current_IP_address()
+    print(has_changed)
     #printnotif = notification("your IP is: " + str(currentip))
-    #print (printnotif)
-    #email= email_notification("your IP is {}".format(get_current_IP_address()), settings)
-    #send(printnotif)
-    #send(email)
-
-
+    #printnotif.send_message()
+    if (has_changed):
+        email = email_notification(currentip,settings["email"])
+        send(email)
+    
 if __name__ == "__main__":
     main()
