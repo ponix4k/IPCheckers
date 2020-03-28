6fa431249de4d5ab6a6596bf825ae96636769837
ponix4k <ponix@protonmail.com>
2020-03-28 18:07:21 +0000

    update files

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-crontab -l > crontab.txt
-echo "### Crontask for IP Checker  ###" >> crontab.txt
-echo "* * * * *  cd ~/IPCheckers && /usr/bin/python3.7 test.py" >> crontab.txt
+#crontab -l > crontab.txt
+#echo "### Crontask for IP Checker  ###" >> crontab.txt
+#echo "* * * * *  cd ~/IPCheckers && /usr/bin/python3.7 test.py" >> crontab.txt
 crontab `pwd`/crontab.txt
 rm `pwd`/crontab.txt
-#sudo apt-get install python3-pip -y
-#pip3 install -r requirements.txt
-
-
+sudo apt-get install python3-pip -y
+pip3 install -r requirements.txt
